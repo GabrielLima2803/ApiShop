8fb8aef1fae93e54c196abf3b23062f8c7537d35
GabrielLima2803 <gabriellima2803@gmail.com>
2023-12-11 19:13:52 -0300

    Create Repository Marca

---
+++ src/main/java/com/example/ApiShop/repositories/MarcaRepository.java
@@ -0,0 +1,11 @@
+package com.example.ApiShop.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.ApiShop.model.Marca;
+
+@Repository
+public interface MarcaRepository extends JpaRepository<Marca, Long> {
+    
+}
