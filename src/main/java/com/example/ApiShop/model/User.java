470a88cdeb1b6a2d79ad169021558ee4e4cc420f
GabrielLima2803 <gabriellima2803@gmail.com>
2023-12-11 19:13:09 -0300

    Create Model User

---
+++ src/main/java/com/example/ApiShop/model/User.java
@@ -0,0 +1,51 @@
+package com.example.ApiShop.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = User.TABLE_NAME)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class User {
+    public static final String TABLE_NAME = "User";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @Column(name = "username", length = 85)
+    @Size(min = 2, max = 85)
+    @NotNull
+    @NotEmpty
+    private String username;
+
+    @Column(name = "password", length = 65)
+    @Size(min = 4, max = 65)
+    @NotNull
+    @NotEmpty
+    private String password;
+
+    @Column(name = "email", length = 255, unique = true)
+    @NotNull
+    @NotEmpty
+    @Email
+    private String email;
+}
