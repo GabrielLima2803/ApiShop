92b50642bcc2f8b009a25121f569576780aa91dc
GabrielLima2803 <gabriellima2803@gmail.com>
2023-12-12 22:41:31 -0300

    Add findByCarrinho

--- src/main/java/com/example/ApiShop/repositories/ItemCarrinhoRepository.java
+++ src/main/java/com/example/ApiShop/repositories/ItemCarrinhoRepository.java
@@ -1,11 +1,15 @@
 package com.example.ApiShop.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.ApiShop.model.Carrinho;
 import com.example.ApiShop.model.ItemCarrinho;
 
 @Repository
 public interface ItemCarrinhoRepository extends JpaRepository<ItemCarrinho, Long> {
-    
+        List<ItemCarrinho> findByCarrinho(Carrinho carrinho);
+
 }
