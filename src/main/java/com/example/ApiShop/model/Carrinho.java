32142abab259c6bf0711dceb2ac0bb070a6eed08
GabrielLima2803 <gabriellima2803@gmail.com>
2023-12-15 19:14:10 -0300

    Fix model cart

--- src/main/java/com/example/ApiShop/model/Carrinho.java
+++ src/main/java/com/example/ApiShop/model/Carrinho.java
@@ -1,9 +1,7 @@
 package com.example.ApiShop.model;
 
 import java.math.BigDecimal;
-import java.util.List;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,7 +9,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -34,14 +31,10 @@ public class Carrinho {
     @Column(name = "id", unique = true)
     private Long id;
 
-    @Column(name = "total_da_compra", precision = 6, scale = 2)
-    private BigDecimal totalDaCompra;
+    @Column(name = "total", scale = 2)
+    private BigDecimal total;
 
     @ManyToOne
     @JoinColumn(name = "foramPagamento_id", nullable = false)
     private FormaDePagamento formaDePagamento;
-
-    @OneToMany(mappedBy = "carrinho", cascade = CascadeType.ALL)
-    private List<ItemCarrinho> itens;
-
 }
