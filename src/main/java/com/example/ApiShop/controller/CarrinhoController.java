ddc2729f1ba975a2442c061beb23d2678c61c4e7
GabrielLima2803 <gabriellima2803@gmail.com>
2023-12-15 19:14:03 -0300

    Logic Carrinho Controller CRUD (Falta ajeitar o add cart)

--- src/main/java/com/example/ApiShop/controller/CarrinhoController.java
+++ src/main/java/com/example/ApiShop/controller/CarrinhoController.java
@@ -1,15 +1,56 @@
 package com.example.ApiShop.controller;
 
+import com.example.ApiShop.model.Carrinho;
+import com.example.ApiShop.service.CarrinhoService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/carrinho")
 @Validated
 public class CarrinhoController {
+
+    @Autowired
+    private CarrinhoService carrinhoService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Carrinho> findById(@PathVariable Long id) {
+        Carrinho obj = carrinhoService.findById(id);
+        return ResponseEntity.ok(obj);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Carrinho>> findAll() {
+        List<Carrinho> carrinhos = carrinhoService.findAll();
+        return ResponseEntity.ok(carrinhos);
+    }
+    @PostMapping("/criar")
+    public ResponseEntity<Carrinho> criarCarrinho(@Valid @RequestBody Carrinho carrinho) {
+        Carrinho savedCarriho = this.carrinhoService.create(carrinho);
+        URI uri = URI.create("/carrinho/" + savedCarriho.getId());
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<Void> update(@Valid @RequestBody Carrinho obj, @PathVariable Long id) {
+        obj.setId(id);
+        obj = this.carrinhoService.update(id, obj);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/excluir/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        carrinhoService.delete(id);
+        return ResponseEntity.noContent().build(); 
+    }
 
 
 }
