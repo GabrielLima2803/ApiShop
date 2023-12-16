86f9eaa54955262310451a818d9ab898a19f4edc
GabrielLima2803 <gabriellima2803@gmail.com>
2023-12-16 11:55:41 -0300

    Add method recalculateTotal in Model Carrinho

--- src/main/java/com/example/ApiShop/model/Carrinho.java
+++ src/main/java/com/example/ApiShop/model/Carrinho.java
@@ -1,7 +1,9 @@
 package com.example.ApiShop.model;
 
 import java.math.BigDecimal;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,6 +11,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -37,4 +40,27 @@ public class Carrinho {
     @ManyToOne
     @JoinColumn(name = "foramPagamento_id", nullable = false)
     private FormaDePagamento formaDePagamento;
+
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+    @OneToMany(mappedBy = "carrinho", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ItemCarrinho> itens;
+
+    public void recalculateTotal() {
+        if (itens != null && !itens.isEmpty()) {
+            BigDecimal novoTotal = BigDecimal.ZERO;
+    
+            for (ItemCarrinho item : itens) {
+                BigDecimal precoProduto = item.getProduto().getPreco();
+                BigDecimal subtotalItem = precoProduto.multiply(BigDecimal.valueOf(item.getQuantidade()));
+                novoTotal = novoTotal.add(subtotalItem);
+            }
+    
+            setTotal(novoTotal);
+        } else {
+            setTotal(BigDecimal.ZERO);
+        }
+    }
 }
