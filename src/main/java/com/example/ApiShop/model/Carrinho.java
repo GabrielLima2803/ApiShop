20b1f980d431fec87e9136fd0e97567f0244954f
GabrielLima2803 <gabriellima2803@gmail.com>
2023-12-17 18:16:29 -0300

    Cleaning Model

--- src/main/java/com/example/ApiShop/model/Carrinho.java
+++ src/main/java/com/example/ApiShop/model/Carrinho.java
@@ -1,14 +1,12 @@
 package com.example.ApiShop.model;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -34,38 +32,18 @@ public class Carrinho {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @JsonProperty("id")
     @Column(name = "id", unique = true)
     private Long id;
 
-    @Column(name = "total", scale = 2)
-    @JsonProperty("total")
+    @Column(name = "total", nullable = false)
     private BigDecimal total;
 
-    @ManyToOne
-    @JoinColumn(name = "formaPagamento_id", nullable = false)  
-    @JsonProperty("formaDePagamento")
-    private FormaDePagamento formaDePagamento;
-
-    @JsonProperty("itens")
-    @OneToMany(mappedBy = "carrinho", fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "carrinho", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ItemCarrinho> itens;
 
-    public void recalculateTotal() {
-        if (itens != null && !itens.isEmpty()) {
-            BigDecimal novoTotal = BigDecimal.ZERO;
-    
-            for (ItemCarrinho item : itens) {
-                if (item.getProduto() != null && item.getProduto().getPreco() != null) {
-                    BigDecimal precoProduto = item.getProduto().getPreco();
-                    BigDecimal subtotalItem = precoProduto.multiply(BigDecimal.valueOf(item.getQuantidade()));
-                    novoTotal = novoTotal.add(subtotalItem);
-                }
-            }
-    
-            setTotal(novoTotal);
-        } else {
-            setTotal(BigDecimal.ZERO);
-        }
-    }
+
+
+    // @ManyToOne
+    // @JoinColumn(name = "formaDePagamento_id", nullable = false)
+    // private FormaDePagamento formaDePagamento;
 }
