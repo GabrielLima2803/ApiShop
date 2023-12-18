7a80c0bf02c840f5692afaa89369599b4612e265
GabrielLima2803 <gabriellima2803@gmail.com>
2023-12-18 16:35:50 -0300

    Fix findByCarrinhoAndProdutoId

--- src/main/java/com/example/ApiShop/repositories/ItemCarrinhoRepository.java
+++ src/main/java/com/example/ApiShop/repositories/ItemCarrinhoRepository.java
@@ -1,8 +1,10 @@
 package com.example.ApiShop.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.ApiShop.model.Carrinho;
@@ -11,5 +13,8 @@ import com.example.ApiShop.model.ItemCarrinho;
 @Repository
 public interface ItemCarrinhoRepository extends JpaRepository<ItemCarrinho, Long> {
         List<ItemCarrinho> findByCarrinho(Carrinho carrinho);
+
+        @Query("SELECT i FROM ItemCarrinho i WHERE i.carrinho = :carrinho AND i.produto.id = :produtoId")
+        Optional<ItemCarrinho> findByCarrinhoAndProdutoId(Carrinho carrinho, Long produtoId);
 
 }
