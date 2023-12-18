e22e3ca2f9cdc0e6851387d5eb1673d9baa4a88b
GabrielLima2803 <gabriellima2803@gmail.com>
2023-12-18 17:33:49 -0300

    Add Role in User Model

--- src/main/java/com/example/ApiShop/model/User.java
+++ src/main/java/com/example/ApiShop/model/User.java
@@ -2,6 +2,8 @@ package com.example.ApiShop.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -48,4 +50,8 @@ public class User {
     @NotEmpty
     @Email
     private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
 }
