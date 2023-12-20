17577e8ea1695dd0a186525c8a2e099437d4d45e
GabrielLima2803 <gabriellima2803@gmail.com>
2023-12-19 21:58:09 -0300

    Update UserRepository

--- src/main/java/com/example/ApiShop/repositories/UserRepository.java
+++ src/main/java/com/example/ApiShop/repositories/UserRepository.java
@@ -2,13 +2,15 @@ package com.example.ApiShop.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import com.example.ApiShop.model.User;
 
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByEmail(String email);
-
+    UserDetails findByEmail(String email);
+    UserDetails findByUsername(String username);
 }
