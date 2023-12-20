3d08ac130e86aff31f5fecc144c2481695fdf21f
GabrielLima2803 <gabriellima2803@gmail.com>
2023-12-20 15:34:36 -0300

    Add username in DtoRegister

--- src/main/java/com/example/ApiShop/model/dtos/RegisterDto.java
+++ src/main/java/com/example/ApiShop/model/dtos/RegisterDto.java
@@ -4,6 +4,6 @@ import com.example.ApiShop.model.UserRole;
 
 import jakarta.validation.constraints.NotNull;
 
-public record RegisterDto(@NotNull String email,@NotNull String password, @NotNull UserRole role ) {
+public record RegisterDto(@NotNull String username, @NotNull String email,@NotNull String password, @NotNull UserRole role ) {
     
 }
